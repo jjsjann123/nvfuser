76256ea0a7f4957430532b4e7850556fdd8a5730
jjsjann123 <jiej@nvidia.com>
2022-11-08 14:23:29 -0800

    removing everything to a toy example

--- ir_all_nodes.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <third_party/nvfuser/ir_base_nodes.h>
-#include <third_party/nvfuser/ir_interface_nodes.h>
-#include <third_party/nvfuser/ir_internal_nodes.h>
-
-// TODO: remove this once the Kernel IR split is complete
-#include <third_party/nvfuser/kernel_ir.h>
--- ops/all_ops.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <third_party/nvfuser/arith.h>
-#include <third_party/nvfuser/ops/alias.h>
-#include <third_party/nvfuser/ops/composite.h>
-#include <third_party/nvfuser/ops/normalization.h>
